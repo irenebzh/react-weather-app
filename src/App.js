3a5ee71c49e58c86570fa80a7cd3374f80a77a8a
irenebzh <128247482+irenebzh@users.noreply.github.com>
2023-09-18 18:03:24 -0500

    Added the GitHub url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="App">
    Weather App
+   <footer>
+    This project was coded by Irina Bozhkova and is {""} 
+   <a href="https://github.com/irenebzh/react-weather-app" target="_blank">open-sourced on GitHub</a>
+   </footer>
     </div>
   );
 }
