8adc294d8c9e8a3b98b0036f79ef80d77c95d94b
irenebzh <128247482+irenebzh@users.noreply.github.com>
2023-09-20 17:32:26 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
    <h1>Weather App</h1>
    <footer>
     This project was coded by Irina Bozhkova and is {""} 
    <a href="https://github.com/irenebzh/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
    </footer>
+    </div>
     </div>
   );
 }
