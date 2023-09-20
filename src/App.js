d91b894e4f778bcba095d07eaebe2109487e888e
irenebzh <128247482+irenebzh@users.noreply.github.com>
2023-09-20 17:54:18 -0500

    Styling HTML

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-   <h1>Weather App</h1>
+   
+   <Weather />
    <footer>
     This project was coded by Irina Bozhkova and is {""} 
    <a href="https://github.com/irenebzh/react-weather-app" target="_blank" rel="noreferrer">open-sourced on GitHub</a>
