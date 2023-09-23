7e163f544bd2c4b7246b37b73ec61dc7010c17da
irenebzh <128247482+irenebzh@users.noreply.github.com>
2023-09-22 19:58:34 -0500

    Weather Icon implementation

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ const [weatherData, setWeatherData] = useState({ ready: false });
     city: response.data.name,
     date: new Date(response.data.dt * 1000),
     description: response.data.weather[0].description,
-    iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    icon: response.data.weather[0].icon
 });       
     }
     function search() {
